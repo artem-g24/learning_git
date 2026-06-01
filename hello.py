1420a6c8f684c6d2a00b28510601cc633e47295d
yummy <agri2026@proton.me>
2026-06-01 11:13:24 +1000

    Add greet function

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@
 print("Goodbye World!\n\n")
 print("Hello I made a change on GitHub!")
 print("Welcome to my script!")
+
+def greet(name):
+    print(f"Hello, {name} made a change on GitHub!")
+
+greet("Artem")
